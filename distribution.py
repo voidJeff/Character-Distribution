0f8de4c19bcec2a83fbd0ec0cca0c76b962ebc08
voidJeff <qy120838@126.com>
2015-09-23 13:40:35 -0400

    Updated from Brython Server: September 23, 2015 1:40:35 PM EDT

--- distribution.py
+++ distribution.py
@@ -37,7 +37,7 @@ Notice about this example:
 * Letters that do not occur in the text are not listed in the output at all.
 """
 import string
-print('The distribution of characters in "{0}" is: '.format(line))
-line = input("Please enter a string of text (the bigger the better): ")
 
+line = input("Please enter a string of text (the bigger the better): ")
+print('The distribution of characters in "{0}" is: '.format(line))
 alphabet = str
