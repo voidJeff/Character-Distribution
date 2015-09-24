41181890d50958742f428b5acb4f6cf93e4d1149
voidJeff <qy120838@126.com>
2015-09-23 21:07:15 -0400

    Updated from Brython Server: 9/23/2015 9:07:15 PM

--- distribution.py
+++ distribution.py
@@ -57,9 +57,7 @@ while n <= 26:
 tupl = zip(lst,qlst)
 tup = list(tupl)
 lnum = len(qlst)
-tup.sort(key=operator.itemgetter(1,0), reverse=False)
-tup.sort(key=operator.itemgetter(1), reverse=True)
-
+tup.sort(key=operator.itemgetter(1,0), reverse=True)
 llst = [x[0] for x in tup]
 qlst.sort()
 
